35dc2762cc7d7578252430699cea74d45447d06a
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-05-08 16:14:40 +0900

    테스트 17

--- scripts/after_deploy.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-# REPOSITORY=/home/ubuntu/myeonjeobi
-# cd $REPOSITORY
-
-# mkdir client/logs
-# mkdir server/logs
-
-# yarn start:all
--- scripts/before_deploy.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-# REPOSITORY=/home/ubuntu/bmart-5
-# cd $REPOSITORY
-
-# mkdir client/logs
-# mkdir server/logs
-
-# yarn start:all
